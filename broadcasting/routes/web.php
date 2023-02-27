a15fe13f94988832b9d089291b3d72f2042d8442
Sarman-Dasa <sarmand@zignuts.com>
2023-02-27 18:21:31 +0530

    broadcasting app

--- broadcasting/routes/web.php
+++ broadcasting/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\MessageNotification;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('event',function(){
+    event(new MessageNotification("hello Gaurav"));
+});
+
+
